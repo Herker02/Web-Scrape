a94c34b7191fa77e15936daa47dbba4d862f4103
HerKer02 <herker02@gmail.com>
2019-08-22 16:20:16 -0500

    UPDATE 2

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var mongoose = require("mongoose")
 var expressHandlebars = require("express-handlebars");
 var bodyParser = require("body-parser");
-var cheerio = require("cheerio");
+
 
 var PORT = process.env.PORT || 3000;
 
